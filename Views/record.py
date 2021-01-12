0344413955548e9c633659f3cec7e205a019f2e7
xiaochen19v587 <1308732441@qq.com>
2021-01-12 20:38:24 +0800

    update

--- Views/record.py
+++ Views/record.py
@@ -1,9 +1,6 @@
 import sys
 import sip
 from PyQt5.QtWidgets import QApplication
-# MAIN_FILE_PATH = os.path.dirname(os.path.abspath(__file__))
-# BASE_PATH = os.path.dirname(MAIN_FILE_PATH)
-# sys.path.append(os.path.join(BASE_PATH, "Ui/"))
 from RecordInfo import Record_Info_Views
 
 if __name__ == "__main__":
