1e7a77a3f99bdd305f5882ad99a6239e838debb0
menghan.wang <1308732441@qq.com>
2021-04-23 11:24:06 +0800

    modify show_test_info func

--- Views/RecordInfo.py
+++ Views/RecordInfo.py
@@ -565,12 +565,9 @@ class Record_Info_Views(QMainWindow, Ui_RecordBug):
             # 如果有测试用例id,在对应控件中显示对应的信息
             for i in range(1, len(self.id_list)):
                 if self.id_list[i] == test_id:
-                    self.textBrowser_3.setText("测试步骤:\n"+self.step_list[i])
-                    self.textBrowser_2.setText("期望结果:\n"+self.result_list[i])
-                    for j in range(0, len(self.case_list[i].split('\n'))):
-                        case_list_data += '\n' + \
-                            self.case_list[i].split('\n')[j]
-                    self.textBrowser.setText("测试用例:"+case_list_data)
+                    self.textBrowser_3.setText("测试步骤:\n" + self.step_list[i])
+                    self.textBrowser_2.setText("期望结果:\n" + self.result_list[i])
+                    self.textBrowser.setText("测试用例:\n" + self.case_list[i])
 
     def err_list(self):
         '''
